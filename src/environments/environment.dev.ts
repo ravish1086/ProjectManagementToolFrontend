33a7b8ce06ef36202f6329591d1796f5603b314d
ravi <ravish1086@gmail.com>
2026-05-01 13:38:31 +0530

    feat: add development environment configuration file with local API endpoint

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,7 +1,7 @@
 export const environment = {
     prod: false,
-    // apiUrl: 'http://localhost:3000',
-    apiUrl:'https://project-management-tool-backend-bay.vercel.app'
+    apiUrl: 'http://localhost:3000',
+    // apiUrl:'https://project-management-tool-backend-bay.vercel.app'
 
     // apiUrl: 'https://trusting-yalow.103-108-220-125.plesk.page'
 
